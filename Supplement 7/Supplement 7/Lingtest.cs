177e0b5fcae00433d002348bccc6650272c6a34d
Willdog34 <145715999+Willdog34@users.noreply.github.com>
2025-02-20 23:39:27 -0500

    Add files via upload
    file for it

---
+++ Supplement 7/Supplement 7/Lingtest.cs
@@ -0,0 +1,67 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Xunit;
+
+Console.WriteLine("Hello, World!");
+
+public class LINQAssignment
+{
+    // Method to return all even numbers with an optional skip count
+    public static IEnumerable<int> GetEvenNumbers(int[] array, int skipCount = 0)
+    {
+        return array.Where(x => x % 2 == 0).Skip(skipCount);
+    }
+
+    // Method to shuffle the array and return odd numbers with an optional skip count
+    public static IEnumerable<int> GetShuffledOddNumbers(int[] array, int skipCount = 0)
+    {
+        Random rng = new Random();
+        var shuffledArray = array.OrderBy(x => rng.Next()).ToArray();
+        return shuffledArray.Where(x => x % 2 != 0).Skip(skipCount);
+    }
+
+    // Method to return the average, minimum, and maximum of the array
+    public static (double Average, int Min, int Max) GetArrayStatistics(int[] array)
+    {
+        return (array.Average(), array.Min(), array.Max());
+    }
+}
+
+// Unit Tests
+public class LINQAssignmentTests
+{
+    private readonly int[] testArray;
+
+    public LINQAssignmentTests()
+    {
+        testArray = Enumerable.Range(0, 1_000_000).ToArray();
+    }
+
+    [Fact]
+    public void TestGetEvenNumbers()
+    {
+        var result = LINQAssignment.GetEvenNumbers(testArray, 5).ToList();
+        Assert.NotEmpty(result);
+        Assert.True(result.First() % 2 == 0);
+    }
+
+    [Fact]
+    public void TestGetShuffledOddNumbers()
+    {
+        var result = LINQAssignment.GetShuffledOddNumbers(testArray, 10).ToList();
+        Assert.NotEmpty(result);
+        Assert.True(result.First() % 2 != 0);
+    }
+
+    [Fact]
+    public void TestGetArrayStatistics()
+    {
+        var (avg, min, max) = LINQAssignment.GetArrayStatistics(testArray);
+        Assert.Equal(499999.5, avg, precision: 1);
+        Assert.Equal(0, min);
+        Assert.Equal(999999, max);
+    }
+}
+
+internal class FactAttribute : Attribute
+{
+}
---
+++ Supplement 7/Supplement 7/LinqFunctions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supplement_7
+{
+    internal class LinqFunctions
+    {
+    }
+}
+
+namespace LinqProject
+{
+    using System;
+    using System.Linq;
+
+    public static class LinqFunctions
+    {
+        public static int[] GetEvenNumbers(int[] array, int skip = 0)
+        {
+            return array.Where(n => n % 2 == 0).Skip(skip).ToArray();
+        }
+
+        public static int[] GetOddNumbersAfterShuffle(int[] array, int skip = 0)
+        {
+            var shuffled = array.OrderBy(_ => Guid.NewGuid()).ToArray();
+            return shuffled.Where(n => n % 2 != 0).Skip(skip).ToArray();
+        }
+
+        public static (double average, int min, int max) GetArrayStatistics(int[] array)
+        {
+            return (array.Average(), array.Min(), array.Max());
+        }
+    }
+}
